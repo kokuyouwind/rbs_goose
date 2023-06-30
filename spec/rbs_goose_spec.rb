f73b9688ecb9606d52044dc6b58e0fdd84c2225b
kokuyouwind <kokuyouwind@gmail.com>
2023-06-30 19:37:35 +0900

    fix rubocop

--- spec/rbs_goose_spec.rb
+++ spec/rbs_goose_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RbsGoose do
   it "has a version number" do
     expect(RbsGoose::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
