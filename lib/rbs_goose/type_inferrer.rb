50b0a624f92681381c4a980093eb293a318392ff
kokuyouwind <kokuyouwind@gmail.com>
2024-01-08 18:19:10 +0900

    API呼び出しをLangChain経由に変更

--- lib/rbs_goose/type_inferrer.rb
+++ lib/rbs_goose/type_inferrer.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 require 'openai'
+require 'langchain'
 
 module RbsGoose
   class TypeInferrer
-    def initialize(llm_client = nil)
-      @llm_client = llm_client || OpenAI::Client.new(access_token: ENV.fetch('OPENAI_ACCESS_TOKEN'))
+    def initialize(llm = nil)
+      @llm = llm || ::Langchain::LLM::OpenAI.new(
+        api_key: ENV.fetch('OPENAI_ACCESS_TOKEN', nil),
+        default_options: {
+          completion_model_name: 'gpt-3.5-turbo-0613',
+          chat_completion_model_name: 'gpt-3.5-turbo-0613'
+        }
+      )
     end
 
     def infer_from_ruby
-      response = llm_client.chat(
-        parameters: {
-          model: 'gpt-3.5-turbo',
-          messages: [{ role: 'user', content: 'Hello!' }],
-          temperature: 0
-        }
-      )
-      response.dig('choices', 0, 'message', 'content')
+      llm.complete(prompt: 'Hello!').completion
     end
 
     private
 
-    attr_reader :llm_client
+    attr_reader :llm
   end
 end
