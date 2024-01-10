2ee06fdae11622f518dc913dac8655c9a99d129d
kokuyouwind <kokuyouwind@gmail.com>
2024-01-11 08:41:42 +0900

    rubocop fixのためexpectedを変数参照に書き換え

--- spec/rbs_goose/file_io_spec.rb
+++ spec/rbs_goose/file_io_spec.rb
@@ -64,16 +64,7 @@ RSpec.describe RBSGoose::FileIO do
       it 'returns ruby markdown' do
         expect(subject).to eq(<<~MARKDOWN.strip)
           ```ruby:user_factory.rb
-          class UserFactory
-            def name(name)
-              @name = name
-              self
-            end
-
-            def build
-              User.new(name: name)
-            end
-          end
+          #{user_factory_code.strip}
           ```
         MARKDOWN
       end
@@ -85,11 +76,7 @@ RSpec.describe RBSGoose::FileIO do
       it 'returns rbs markdown' do
         expect(subject).to eq(<<~MARKDOWN.strip)
           ```rbs:user_factory.rbs
-          class UserFactory
-            def name: (untyped name) -> untyped
-
-            def build: () -> untyped
-          end
+          #{user_factory_rbs.strip}
           ```
         MARKDOWN
       end
