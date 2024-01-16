4205c14c42100a9f882b2a611631df1048218f46
kokuyouwind <kokuyouwind@gmail.com>
2024-01-16 21:51:15 +0900

    factoryを使ってOneByOneTemplate specをリファクタリング

--- spec/rbs_goose/templates/one_by_one_template_spec.rb
+++ spec/rbs_goose/templates/one_by_one_template_spec.rb
@@ -1,64 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe RbsGoose::Templates::OneByOneTemplate do
-  let(:example_ruby) do
-    RbsGoose::IO::File.new(
-      path: 'example_ruby.rb',
-      content: <<~RUBY
-        example_ruby_line_1
-        example_ruby_line_2
-        example_ruby_line_3
-      RUBY
-    )
+  let(:examples) do
+    [{
+      typed_ruby: build(
+        :typed_ruby,
+        ruby: build(:file, :with_multi_line_content, line_prefix: 'example_ruby_line_', path: 'example_ruby.rb'),
+        rbs: build(:file, :with_multi_line_content, line_prefix: 'example_rbs_line_', path: 'example_rbs.rbs')
+      ),
+      refined_rbs: build(
+        :file, :with_multi_line_content,
+        line_prefix: 'example_refined_rbs_line_',
+        path: 'example_rbs.rbs'
+      )
+    }]
   end
 
-  let(:example_rbs) do
-    RbsGoose::IO::File.new(
-      path: 'example_rbs.rbs',
-      content: <<~RBS
-        example_rbs_line_1
-        example_rbs_line_2
-        example_rbs_line_3
-      RBS
+  let(:input_typed_ruby) do
+    build(
+      :typed_ruby,
+      ruby: build(:file, :with_multi_line_content, line_prefix: 'input_ruby_line_', path: 'input_ruby.rb'),
+      rbs: build(:file, :with_multi_line_content, line_prefix: 'input_rbs_line_', path: 'input_rbs.rbs')
     )
   end
-
-  let(:example_refined_rbs) do
-    RbsGoose::IO::File.new(
-      path: 'example_rbs.rbs',
-      content: <<~RBS
-        example_refined_rbs_line_1
-        example_refined_rbs_line_2
-        example_refined_rbs_line_3
-      RBS
-    )
-  end
-
-  let(:input_ruby) do
-    RbsGoose::IO::File.new(
-      path: 'input_ruby.rb',
-      content: <<~RUBY
-        input_ruby_line_1
-        input_ruby_line_2
-        input_ruby_line_3
-      RUBY
-    )
-  end
-
-  let(:input_rbs) do
-    RbsGoose::IO::File.new(
-      path: 'input_rbs.rbs',
-      content: <<~RBS
-        input_rbs_line_1
-        input_rbs_line_2
-        input_rbs_line_3
-      RBS
-    )
-  end
-
-  let(:input_typed_ruby) { RbsGoose::IO::TypedRuby.new(ruby: input_ruby, rbs: input_rbs) }
-  let(:example_typed_ruby) { RbsGoose::IO::TypedRuby.new(ruby: example_ruby, rbs: example_rbs) }
-  let(:examples) { [{ typed_ruby: example_typed_ruby, refined_rbs: example_refined_rbs }] }
 
   describe '#format' do
     subject { described_class.new(instruction: instruction, examples: examples).format(input_typed_ruby) }
