4ef699ae40a0e0dc836c9b98f066f866eb1d9344
kokuyouwind <kokuyouwind@gmail.com>
2024-04-20 19:03:18 +0900

    add infer_spec

---
+++ spec/rbs_goose/orthoses/infer_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+RSpec.describe RbsGoose::Orthoses::Infer do
+  describe '#initialize' do
+    subject { described_class.new(nil) }
+
+    it 'sets default code_dir' do
+      expect(subject.instance_variable_get(:@code_dir)).to eq('lib')
+    end
+
+    it 'sets default sig_dir' do
+      expect(subject.instance_variable_get(:@sig_dir)).to eq('sig')
+    end
+
+    context 'when block is given' do
+      subject do
+        described_class.new(nil) do |config|
+          config.use_open_ai('open_ai_access_token')
+        end
+      end
+
+      it 'sets OpenAI llm' do
+        subject
+        expect(RbsGoose.configuration.llm).to be_a(Langchain::LLM::OpenAI)
+      end
+    end
+  end
+
+  describe '#call' do
+    subject { described_class.new(nil) }
+
+    it 'calls RbsGoose.run' do
+      allow(RbsGoose).to receive(:run)
+      subject.call
+      expect(RbsGoose).to have_received(:run).with(code_dir: 'lib', sig_dir: 'sig')
+    end
+  end
+end
